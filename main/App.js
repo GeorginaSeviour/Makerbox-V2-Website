197fbe40d3ee9ad425b1f8ec1bf0588f728c687b
nathan-bird <nathan2484@hotmail.co.uk>
2016-08-09 21:29:16 +0200

    webpack setup

---
+++ main/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import styles from './App.css';
+
+import Image from '../src/Image';
+
+export default class App extends React.Component {
+
+  handleImageClick() {
+    console.log('You have clicked the image!!');
+  }
+
+  render() {
+    return (
+      <div className={styles.wrapper}>
+        <Image
+          imageUrl={'https://s-media-cache-ak0.pinimg.com/564x/b5/b4/ac/b5b4ac05185581180a2e27bb66fddf54.jpg'}
+          onClick={this.handleImageClick}
+          />
+      </div>
+    );
+  }
+}
