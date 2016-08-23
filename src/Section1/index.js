eeb52d12162c9e5b2a9982cc913223901aa26087
nathan-bird <nathan2484@hotmail.co.uk>
2016-08-23 22:54:35 +0200

    button and text

--- src/Section1/index.js
+++ src/Section1/index.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import {PropTypes} from 'react';
+import {Button} from 'react-bootstrap';
 import classNames from 'classnames';
 import styles from './styles.css';
 
 function Section1(props) {
   return (
     <div className={styles.backgroundImage}>
-      <h1>
-        Makerbox 
+      <h1 className={styles.logo}>
+        Makerbox
       </h1>
+      <div
+        className={styles.buttonContainer}
+        >
+        <Button
+          bsSize={'large'}
+          className={styles.playButton}
+          >
+          {'Play'}
+        </Button>
+
+        <span className={styles.slogan}>
+          {'Let´s revolutionize the way engineering and problem solving is experienced.'}
+        </span>
+
+      </div>
     </div>
   );
 }
